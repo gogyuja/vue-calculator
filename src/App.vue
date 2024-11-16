1aa63e48217b952046d3fbd25b8c1f6842ab2332
gogyuja <gogyuja@gmail.com>
2024-11-16 11:30:45 +0900

    버튼 클래스바인딩구현

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import Btn from './components/Btn.vue';
 </script>
 
 <template>
   <body>
       <h1>계산기</h1>
+      <Btn :class="`number`">1</Btn>
+      <Btn :class="`operator`">+</Btn>
     </body>
 </template>
 
--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -1,11 +1,21 @@
 <template>
-  
+<body>
+  <button :class="class"><slot></slot></button>
+</body>
 </template>
 
 <script setup lang='ts'>
-
+defineProps({
+  class: {
+    type: String,
+    required: true,
+    validator: (value: string) => ['number', 'operator'].includes(value),
+  },
+});
 </script>
 
 <style>
 
+
 </style>
+<style scoped src="/src/assets/btn.css"></style>
