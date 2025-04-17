f12c02cd0fb21be869068f178763c3cc7421cf38
gogyuja <gogyuja@gmail.com>
2025-04-17 15:05:33 +0900

    playwright 간단 사용

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,8 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
+  await page.goto('/')
+  await expect(page.locator('div > h1')).toHaveText('계산기')
+  await page.keyboard.press('1')
+  await page.keyboard.press('+')
+  await page.keyboard.press('2')
+  await page.keyboard.press('Enter')
+  await expect(page.locator('div.inputarea')).toHaveText('3')
 })
